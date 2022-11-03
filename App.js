9be6668ca2f928e53c1453decd802ec80456a221
Paloma St.Clair <palomast.clair@10-21-83-75.dynapool.wireless.nyu.edu>
2022-11-03 02:08:51 -0400

    horse images working

--- App.js
+++ App.js
@@ -5,5 +5,5 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send("hello world")
 });
-
+app.use('/images', express.static('public'));
 app.listen(port, () => console.log('slay its working'));
